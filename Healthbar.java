af99e00dc3cf825e5770c278c2015957dbe41273
vicperezch <vmpch1@outlook.com>
2023-07-19 16:11:02 -0600

    Clases e imagenes añadidas

---
+++ Healthbar.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Healthbar here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Healthbar extends Actor
+{
+    /**
+     * Act - do whatever the Healthbar wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
